0a92efd191f0d2f1da3f87cb8ef953688b0f150b
Divy <bhawnayadav@Bhawnas-MacBook-Air.local>
2025-08-05 01:04:54 +0530

    made changes necessary

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -72,8 +72,10 @@ def apply_bow(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: i
 
         test_df = pd.DataFrame(X_test_bow.toarray())
         test_df['label'] = y_test
+        
+        with open('vectorizer.pkl', 'wb') as file:
+            pickle.dump(vectorizer, file)
 
-        pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb'))
         logger.debug('Bag of Words applied and data transformed')
         return train_df, test_df
     except Exception as e:
